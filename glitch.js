4e42d734d9d04c0710b81b6e7ceebddc8e4982c7
Plume <anascroll@hotmail.fr>
2021-08-29 05:39:29 +0200

    Create glitch.js

---
+++ glitch.js
@@ -0,0 +1,17 @@
+let canva = document.getElementById('noise');
+let count = 20;
+for (let i =0; i < count; i++){
+	let glitchbox = document.createElement('div')
+		glitchbox.className = 'gbox';
+		canva.appendChild(glitchbox);
+}
+setInterval(function(){
+let glitch =  document.getElementsByClassName('gbox');
+for (let i = 0; i < glitch.length; i++){
+	glitch[i].style.left = Math.floor(Math.random()*100) + 'vw';
+	glitch[i].style.top = Math.floor(Math.random()*100) + 'vh';
+	glitch[i].style.width = Math.floor(Math.random()*400) + 'px';
+	glitch[i].style.height = Math.floor(Math.random()*100) + 'px';
+	glitch[i].style.backgroundPosition = Math.floor(Math.random()*50) + 'px';
+}
+}, 200)
